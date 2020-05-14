ffd830044d810a0a95cdd60769fc98f5e3c56b1b
shompa <nico_nahuelpan@hotmail.com>
2020-05-14 17:11:19 -0400

    base landing template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { BannerComponent } from './pages/banner/banner.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { ClientsComponent } from './pages/clients/clients.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { SocialComponent } from './pages/social/social.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    AboutComponent,
+    ServicesComponent,
+    ClientsComponent,
+    PortfolioComponent,
+    ContactComponent,
+    SocialComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
